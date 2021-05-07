73483727d8f9b06d86a65044a823d27ec5c47c8b
rzp <1019394597@qq.com>
2021-05-07 22:36:18 +0800

    传递闭包

---
+++ Algorithm4/Digraph/TransitiveClosure.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digraph
+{
+    /// <summary>
+    /// 传递闭包解决有向图顶点对的可达性
+    /// </summary>
+    class TransitiveClosure
+    {
+        private DirectedDFS[] all;
+        public TransitiveClosure(Digraph digraph)
+        {
+            this.all = new DirectedDFS[digraph.Vertices()];
+            for (int i = 0; i < digraph.Vertices(); i++)
+            {
+                all[i] = new DirectedDFS(digraph, i);
+            }
+        }
+
+        public bool Reachable(int v, int w)
+        {
+            return all[v].Marked(w);
+        }
+    }
+}
